4366d0f5d2589fe51e34e74cb33e5c275008f5dc
Brian Huston <hbriandrums@gmail.com>
2018-10-30 14:45:25 -0700

    Simple fix for react test

--- spec/react.test.js
+++ spec/react.test.js
@@ -5,6 +5,9 @@ import App from '../client/src/App.jsx';
 const wrapper = shallow(<App />);
 
 test('it checks to see if an h2 exists', () => {
-  expect(wrapper.find('h2').length !== 0).toBe(true);
+  expect(wrapper.find('h2').length !== 0).toBe(true); 
+});
+
+test('it checks to see if an li does not', () => {
   expect(wrapper.find('li').length !== 0).toBe(false); 
 });
