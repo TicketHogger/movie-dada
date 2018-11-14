3c607b3d637dfbddcbf2227c79b65f56c5a7f463
Mary Matthews <matthewsmp@gmail.com>
2018-11-13 21:42:48 -0800

    begin PUT route

--- noSQL/database/connection.js
+++ noSQL/database/connection.js
@@ -15,4 +15,31 @@ const getAll = (query, callback) => {
   });
 };
 
-module.exports.getAll = getAll;
+const addOne = (title, year, image, genre, callback) => {
+
+  // console.log('year>>>>>>>>>>>>>>>>>>>>>>', year);
+  const queryStr = `INSERT INTO movies (title, year, image, genre) VALUES (?, ?, ?, ?)`;
+
+  connection.query(queryStr, [title, year, image, genre], (error, results) => {
+    if (error) {
+      callback(error);
+    } else {
+      callback(null, results);
+    }
+  });
+
+};
+
+const updateMovieData = (id, title, year, image, genre, callback) => {
+
+  // UPDATE movies SET genre = 'romance' WHERE genre = 'mary';
+  if (title) {
+    connection.query(`UPDATE movies SET title = ? WHERE genre = ?`, [id, title], (err, results) => {
+
+    })
+  }
+
+
+};
+
+module.exports = { getAll, addOne, updateMovieData };
--- noSQL/server/index.js
+++ noSQL/server/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
-
 const path = require('path');
-
+const bodyParser = require('body-parser');
 const db = require('../database/connection.js');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
 // TO DO: Extend the existing API to support all CRUD operations:
 // Create / POST - create a new item
@@ -16,7 +18,7 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/api/movies/:genre/relatedmovies', (req, res) => {
   const param = req.params.genre;
-  console.log(param);
+
   db.getAll(param, (error, results) => {
     if (error) {
       res.status(500).send(error.message);
@@ -26,10 +28,9 @@ app.get('/api/movies/:genre/relatedmovies', (req, res) => {
   });
 });
 
-app.post('/api/movies/:movieId/relatedmovies', (req, res) => {
-  const movieId = req.params.movieId;
-
-  db.addOne(movieId, (error, results) => {
+app.post('/api/movies', (req, res) => {
+  // console.log('req from index.js line 30>>>>>>>>>>>>>>>>>', req.body);
+  db.addOne(req.body.title, req.body.year, req.body.image, req.body.genre, (error, results) => {
     if (error) {
       res.status(500).send(error.message);
     } else {
@@ -38,6 +39,14 @@ app.post('/api/movies/:movieId/relatedmovies', (req, res) => {
   });
 });
 
-
+// app.put('/api/movies', (req, res) => {
+//   db.updateMovieData(req.body.movieId, req.body.title, req.body.year, req.body.image, req.body.genre, (error, results) => {
+//     if (error) {
+//       res.status(500).send(error.message);
+//     } else {
+//       res.status(201).send();
+//     }
+//   });
+// });
 
 module.exports = app;
