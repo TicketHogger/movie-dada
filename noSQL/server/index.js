573c59ad53af442676d7e526211da2072e8fbafc
Mary Matthews <matthewsmp@gmail.com>
2018-11-13 19:55:23 -0800

    fix get route param and add post route

--- noSQL/server/index.js
+++ noSQL/server/index.js
@@ -8,8 +8,15 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
+// TO DO: Extend the existing API to support all CRUD operations:
+// Create / POST - create a new item
+// Read / GET - read an item
+// Update / PUT - update an item
+// Delete / DELETE - delete an item
+
 app.get('/api/movies/:genre/relatedmovies', (req, res) => {
-  const param = req.params.genre.slice(1);
+  const param = req.params.genre;
+  console.log(param);
   db.getAll(param, (error, results) => {
     if (error) {
       res.status(500).send(error.message);
@@ -18,5 +25,19 @@ app.get('/api/movies/:genre/relatedmovies', (req, res) => {
     }
   });
 });
+
+app.post('/api/movies/:movieId/relatedmovies', (req, res) => {
+  const movieId = req.params.movieId;
+
+  db.addOne(movieId, (error, results) => {
+    if (error) {
+      res.status(500).send(error.message);
+    } else {
+      res.status(201).send();
+    }
+  });
+});
+
+
 
 module.exports = app;
