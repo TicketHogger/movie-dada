1cfb530a0c8cf072d2e60ec7e427d6365edb0838
Mary Matthews <matthewsmp@gmail.com>
2018-11-16 14:26:12 -0800

    create data generation script

--- database/faker.js
+++ database/faker.js
@@ -1,32 +1,59 @@
-// NOTES:
-// my notes test 
-
 const faker = require('faker');
 const fs = require('fs');
 
 const randomInt = max => Math.floor(Math.random() * max + 1);
-
 const randNum = max => Math.floor(Math.random() * max);
-
-fs.writeFile('database/movie.csv', 'id,title,year,image,genre', (err) => {
-  if (err) throw err;
-});
-
 const genres = ['action', 'romance', 'comedy', 'horror', 'documentary'];
 
-for (let i = 1; i < 101; i += 1) {
-  let year = faker.date.future();
-  year = Number(JSON.stringify(year).slice(1, 5));
+const file = fs.createWriteStream('database/movie.csv');
+file.write('id,title,year,image,genre\n');
 
-  const movie = {
-    title: faker.lorem.words(),
-    year,
-    image: `https://s3-us-west-1.amazonaws.com/moovi-images/${randomInt(20)}.jpeg`,
-    genre: genres[randNum(genres.length)],
-  };
+function writeOneMillionTimes() {
+  let i = 10000001;
 
-  fs.appendFile('database/movie.csv', `\n"${i}","${movie.title}",${movie.year},"${movie.image}","${movie.genre}"`, (err) => {
-    if (err) throw err;
-    console.log('Saved!');
-  });
+  write();
+  function write() {
+    let ok = true;
+    do {
+      i--;
+      if (i === 1) {
+        // last time!
+
+        let year = faker.date.between('1970-01-01', '2019-01-01');
+        year = Number(JSON.stringify(year).slice(1, 5));
+        const movie = {
+          title: faker.lorem.words() + i,
+          year,
+          image: `https://s3.us-east-2.amazonaws.com/ticket-hogger/movie-posters/movieImg${randomInt(299)}.png`,
+          genre: genres[randNum(genres.length)],
+        };
+
+        file.write(`"${i}","${movie.title}",${movie.year},"${movie.image}","${movie.genre}"\n`, () => {
+          console.log('wrote one');
+          console.clear();
+        });
+      } else {
+        // see if we should continue, or wait
+        // don't pass the callback, because we're not done yet.
+
+        let year = faker.date.between('1970-01-01', '2019-01-01');
+        year = Number(JSON.stringify(year).slice(1, 5));
+        const movie = {
+          title: faker.lorem.words() + i,
+          year,
+          image: `https://s3.us-east-2.amazonaws.com/ticket-hogger/movie-posters/movieImg${randomInt(299)}.png`,
+          genre: genres[randNum(genres.length)],
+        };
+
+        ok = file.write(`"${i}","${movie.title}",${movie.year},"${movie.image}","${movie.genre}"\n`);
+      }
+    } while (i > 1 && ok);
+    if (i > 1) {
+      // had to stop early!
+      // write some more once it drains
+      file.once('drain', write);
+    }
+  }
 }
+
+writeOneMillionTimes();
